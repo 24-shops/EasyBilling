277eb5554e4beefb3961341a0e4abd50e69d0fb7
shop-go <24-shops@mail.ru>
2017-03-19 10:03:37 +0500

    add function
    add function

--- PaymentAPI.php
+++ PaymentAPI.php
@@ -2,9 +2,15 @@
 
 class PaymentAPI
 {
+	const STATUS_OK = 'ok';
+	const STATUS_FAIL = 'fail';
+	
 	protected
 	// Индификатор платежа
 	$payment_id,
+	
+	// Дата создания платежа
+	$payment_ts,
 	
 	// От кого
 	$user_id_from,
@@ -27,5 +33,18 @@ class PaymentAPI
 	function getAmount()
 	{
 		return $this->amount;
+	}
+	
+	/**
+	 * Сформировать информацию
+	 */
+	function getPayment()
+	{
+		$values = [
+			$this->payment_id,
+			date("Y-m-d H:i:s", $this->payment_ts),
+			$this->getAmount(),
+		];
+		return sprintf("№%s от %s на сумму %s", $values[0], $values[1], $values[2]);
 	}
 }
