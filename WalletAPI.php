cebaa93376bf6558c0b0e9e521261b3b2d211d76
shop-go <24-shops@mail.ru>
2017-03-19 11:25:33 +0500

    add file
    add file

---
+++ WalletAPI.php
@@ -0,0 +1,30 @@
+<?php
+
+class WalletAPI
+{
+	protected
+	// Индификатор пользователя
+	$user_id,
+	
+	// Кошелек
+	$wallet,
+	
+	// Баланс
+	$balance,
+	
+	// Валюта
+	$currency_id,
+	
+	// Активно?
+	$isActive;
+	
+	function WalletAPI()
+	{
+	}
+	
+	/**
+	 * Получить все кошелки по индификатору пользователя
+	 */
+	function getWallets(Database $database, $user_id) {
+	}
+}
